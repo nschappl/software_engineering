34b657c19dbb4feee2ad74ac9c046b42a5d25fbf
jgiunco <john@john-virtual-machine.(none)>
2012-11-04 18:13:46 -0500

    Add repo manager #9

---
+++ repo_manager.php
@@ -0,0 +1,61 @@
+<?php
+  require_once("./include/membersite_config.php");
+
+  if(!$fgmembersite->CheckLogin())
+  {
+      $fgmembersite->RedirectToURL("login.php");
+      exit;
+  }
+ 
+  require_once("./include/gitclass_config.php");
+  
+  if(isset($_POST['submitted'])) {
+
+       if($fggitclass->addRepo())
+       {
+	  $fgmembersite->RedirectToURL("repo_manager.php");
+       }
+  
+  }
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Repo Manager</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+   </head>
+  <body>
+  <h2>Project Manager</h2>
+  <h3>Add a Repo</h3>
+    <form class="form-inline" action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' />
+      <input type='hidden' name='submitted' id='submitted' value='1'/>
+      <input type="text" class="input-large" name='repo_url' id='repo_url' placeholder="HTTP Repo URL" />
+      <button type="submit" class="btn">Add</button>
+    </form>
+    
+
+    
+    <?php $repos = $fggitclass->getRepos();
+    
+    echo     '<h3>Tracked Repos</h3>';
+       
+     echo '<table class="table table-bordered">';
+     echo '<tr><td>Repo ID</td><td>Repo URL</td></tr>';
+     foreach ($repos as $repo) {
+      echo '<tr>';
+	echo '<td>'.$repo['id_repo'].'</td>';
+	echo '<td>'.$repo['name'].'</td>';
+      echo '</tr>';
+     }
+     
+     echo '</table>';
+    
+    ?>
+    
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
